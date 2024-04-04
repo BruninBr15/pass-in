57ca3fc4e959f26d67f5ad1b3344ff8b4a4e58b1
BruninBr15 <bruninbr1510@gmail.com>
2024-04-04 12:54:15 -0300

    Preparação para o teste de GET e POST com Insomnia.

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -3,10 +3,16 @@ package rocketseat.com.passin.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import lombok.RequiredArgsConstructor;
+import rocketseat.com.passin.dto.event.EventIdDTO;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
 import rocketseat.com.passin.services.EventService;
 
 @RestController
@@ -17,9 +23,19 @@ public class EventController {
     private final EventService service;
 
     @GetMapping("/{eventId}")
-    public ResponseEntity<String> getEvent(@PathVariable String eventId) {
-        this.service.getEventDetail(eventId);
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId) {
+        EventResponseDTO event = this.service.getEventDetail(eventId);
         ;
-        return ResponseEntity.ok("Hello World!");
+        return ResponseEntity.ok(event);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body,
+            UriComponentsBuilder uriComponentsBuilder) {
+        EventIdDTO eventIdDTO = this.service.createEvent(body);
+
+        var uri = uriComponentsBuilder.path("/events/{eventId}").buildAndExpand(eventIdDTO.eventId()).toUri();
+
+        return ResponseEntity.created(uri).body(eventIdDTO);
     }
 }
